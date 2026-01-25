bffdddbe13293c7d125e143be8d103acafd0a663
LeonardRuhmann <LeonardRuhmann@gmail.com>
2026-01-25 19:48:45 -0300

    feat: Implement pre/post-processing to improve PER, ORG, AND LOC accuracy

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import spacy
+from utils import clean_text, is_valid_entity
 
 # Initialize the API
 app = FastAPI(title="Vitae-I API", version="1,0")
@@ -11,9 +12,9 @@ class ResumeText(BaseModel):
     text: str
 
 # This fuction run the model.
-def load_model():
+def load_model_with_ruler():
     nlp = spacy.load("pt_core_news_lg")
-    ruler = nlp.add_pipe("entity_ruler", before="ner")
+    ruler = nlp.add_pipe("entity_ruler", config={"overwrite_ents": False}, before="ner")
 
     skills_list = [
             "Python", "Java", "C++", "C#", "JavaScript", "TypeScript", "HTML", "CSS",
@@ -21,21 +22,36 @@ def load_model():
             "Streamlit", "SQL", "MySQL", "PostgreSQL", "MongoDB", "Docker", 
             "Kubernetes", "AWS", "Azure", "GCP", "Git", "GitHub", "Linux",
             "Machine Learning", "Deep Learning", "NLP", "SpaCy", "Scikit-Learn",
-            "Pandas", "NumPy", "TensorFlow", "PyTorch"
+            "Pandas", "NumPy", "TensorFlow", "PyTorch", "Inglês", "Espanhol", "Francês", "Alemão"
         ]
 
-    patterns = [{"label": "SKILL", "pattern": [{"LOWER": skill.lower()}]} for skill in skills_list]
+    patterns = []
+    for skill in skills_list:
+        toks = [tok.lower() for tok in skill.split()]
+        pattern = [{"LOWER": t} for t in toks]
+        patterns.append({"label": "SKILL", "pattern": pattern})
+
+    organizations = ["Banco do Brasil", "Universidade Federal de Minas Gerais", "UFMG"]
+    for org in organizations:
+        toks = [tok.lower() for tok in org.split()]
+        patterns.append({"label": "ORG", "pattern": [{"LOWER": t} for t in toks]})
+    
+    locations = ["Belo Horizonte", "São Paulo"]
+    for loc in locations:
+        patterns.append({"label": "GPE", "pattern": [{"LOWER": t} for t in loc.split()]})
 
     ruler.add_patterns(patterns)
 
     return nlp
 
-nlp = load_model()
+nlp = load_model_with_ruler()
 
 @app.post("/analyze")
 async def analyze_resume(resume: ResumeText):
+
+    processed_text = clean_text(resume.text)
     # Process the text
-    doc = nlp(resume.text)
+    doc = nlp(processed_text)
 
     # Extract the data
     skills = []
@@ -46,6 +62,7 @@ async def analyze_resume(resume: ResumeText):
             if ent.text not in skills:
                 skills.append(ent.text)
         else:
-            other_entities.append({"text": ent.text, "label": ent.label_})
+            if is_valid_entity(ent.text, ent.label_):
+                other_entities.append({"text": ent.text, "label": ent.label_})
 
     return { "skills": skills, "entities": other_entities }
